0f9cd7f353b42b0751d6225830c009ff66a2818b
Kissa <Himasa9@users.noreply.github.com>
2022-03-09 16:01:36 +0900

    修正

--- src/main/java/com/example/demo/WordController.java
+++ src/main/java/com/example/demo/WordController.java
@@ -4,9 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
 import java.util.List;
 
 @Controller
@@ -20,7 +17,7 @@ public class WordController {
 
         if(wordForm.getId() != null) {
             EnglishWord englishWord = wordsService.findById(wordForm.getId());
-            model.addAttribute("EnglishWord", englishWord);
+            model.addAttribute("englishWord", englishWord);
         }
 
         if(showList != null) {
